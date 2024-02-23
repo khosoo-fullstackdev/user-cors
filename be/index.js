476a4e31656796e0de607e6b14ba4d086b15133b
Angi <khsoo.fsdev@gmail.com>
2024-02-23 09:28:27 +0800

    asdfg

--- be/index.js
+++ be/index.js
@@ -1,17 +1,20 @@
+// module
+// import bla from "bla bla";
+
+// common js
+// const bla = require("bla bla");
+
 const express = require("express");
 const cors = require("cors");
-const dummyData = require("./dummy.json");
-const app = express();
 const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.use(cors());
+const { userNames } = require("./dummy.json");
 const fs = require("fs");
 
-app.get("/users", (req, res) => {
-  res.type("application.json");
-  res.send(dummyData);
-  console.log("typeof dummyData: ", typeof dummyData);
-});
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
 
 app.get("/userNames", (request, response) => {
   response.type = "application/json";
@@ -48,41 +51,34 @@ app.post("/add-user", (req, res) => {
     }
   );
 });
+// app.post("delete-user", (req, res) => {
+//   const idToDelete = req.body.id;
+//   fs.writeFile("dummy.json", JSON.stringify(jsonFile), (err) => {
+//     if (err) {
+//       console.log(err);
+//       res.send("error happened");
+//     } else {
+//       console.log("success");
+//       res.send("User added successfully");
+//     }
+//   });
+// });
 
-app.post("/delete-user", (req, res) => {
-  let deletedDummy = dummyData.users.filter((user) => {
-    return user.id != req.body.id;
-  });
+// app.post("update-user", (req, res) => {
+//   const { id, updateData } = re.body;
+//   fs.writeFile("dummy.json", JSON.stringify(jsonFile), (err) => {
+//     if (err) {
+//       console.log(err);
+//       res.send("error happened");
+//     } else {
+//       console.log("success");
+//       res.send("User added successfully");
+//     }
+//   });
+// });
 
-  fs.writeFile("dummy.json", JSON.stringify({ users: deletedDummy }), (err) => {
-    if (err) {
-      console.log(err);
-      res.send("Error happened when write file");
-    } else {
-      console.log("dummyData: ", dummyData);
-      res.send("dummyData changed success");
-    }
-  });
-});
+// const updateData = { ...newUser, ...updateUser };
 
-app.post("/edit-user", (req, res) => {
-  let editedDummy = dummyData.users.map((user) => {
-    if (user.id == req.body.id) {
-      return user.id != req.body.id;
-    }
-  });
-
-  fs.writeFile("dummy.json", JSON.stringify({ users: editedDummy }), (err) => {
-    if (err) {
-      console.log(err);
-      res.send("Error happened when write file");
-    } else {
-      console.log("dummyData: ", dummyData);
-      res.send("dummyData changed success");
-    }
-  });
-});
-
-app.listen("4001", () => {
-  console.log("Server is listening at port 4001");
+app.listen(3001, () => {
+  console.log("Server is listening at port 3001");
 });
